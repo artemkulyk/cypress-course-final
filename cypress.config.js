7f7a4e338f9ea5a2d25059276aa53651db20c3ad
Artem Kulyk <artem.kulyk@gmail.com>
2025-08-19 15:58:51 +0200

    Enable studio

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'g5c7xo',
   e2e: {
+    experimentalStudio: true,
     baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
