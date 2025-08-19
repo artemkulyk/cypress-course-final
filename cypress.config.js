6b79fe079f9b8f718db044d415a31567c73ce00a
Artem Kulyk <artem.kulyk@gmail.com>
2025-08-19 12:01:25 +0200

    Cypress projectId added

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'a7bq2k',
   e2e: {
     baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
