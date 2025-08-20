63b33570b9d93c8b9f505c1075f6f0ee527171dc
Artem Kulyk <artem.kulyk@gmail.com>
2025-08-20 11:20:17 +0200

    add test error

--- cypress/e2e/todo_test.cy.js
+++ cypress/e2e/todo_test.cy.js
@@ -19,4 +19,20 @@ describe('template spec', () => {
 
     cy.get('.new-task-form > label').contains('task');
   });
+
+  /* ==== Test Created with Cypress Studio ==== */
+  it('new_test', function() {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('https://microsoftedge.github.io/Demos/demo-to-do/');
+    cy.get('#new-task').clear('t');
+    cy.get('#new-task').type('task{enter}');
+    cy.get('[type="submit"]').click();
+    cy.get('#new-task').clear('a');
+    cy.get('#new-task').type('another{enter}');
+    cy.get('[type="submit"]').click();
+    cy.get(':nth-child(2) > label > .box').check();
+    /* ==== End Cypress Studio ==== */
+
+    cy.get('.new-task-form > label').should('have.text', '➕ Add task');
+  });
 })
